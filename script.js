2b9a7787cd6da809184849acd35f10ea39775baa
Fotios Zachopoulos <65657774+zachfotis@users.noreply.github.com>
2021-09-11 12:17:50 +0300

    Initial Commit

---
+++ script.js
@@ -0,0 +1,71 @@
+const video = document.getElementById('video');
+const play = document.getElementById('play');
+const stopp = document.getElementById('stop');
+const progress = document.getElementById('progress');
+const timestamp = document.getElementById('timestamp');
+let isMouseDown = false;
+
+// Event listeners
+
+video.addEventListener('click', toggleVideoStatus);
+video.addEventListener('pause', updatePlayIcon);
+video.addEventListener('play', updatePlayIcon);
+video.addEventListener('timeupdate', updateProgress);
+
+play.addEventListener('click', toggleVideoStatus);
+
+stopp.addEventListener('click', stopVideo);
+
+progress.addEventListener('change', setVideoProgress);
+progress.addEventListener('mousedown', () => { isMouseDown = true });
+progress.addEventListener('mouseup', () => { isMouseDown = false });
+
+// Play and pause video
+function toggleVideoStatus() {
+    if (video.paused) {
+        video.play();
+    } else {
+        video.pause();
+    }
+}
+
+// Update play/pause icon
+function updatePlayIcon() {
+    if (video.paused) {
+        play.innerHTML = '<i class="fa fa-play fa-2x"></i>';
+    } else {
+        play.innerHTML = '<i class="fa fa-pause fa-2x"></i>';
+    }
+}
+
+// Update progress and timestamp
+function updateProgress() {
+    if (!isMouseDown) {
+        progress.value = (video.currentTime / video.duration) * 100;
+    }
+
+    // Get minutes
+    let mins = Math.floor(video.currentTime / 60);
+    if (mins < 10) {
+        mins = '0' + mins;
+    }
+    // Get seconds
+    let secs = Math.floor(video.currentTime % 60);
+    if (secs < 10) {
+        secs = '0' + secs;
+    }
+
+    timestamp.innerHTML = mins + ":" + secs;
+
+}
+
+// Set video time to progress
+function setVideoProgress() {
+    video.currentTime = (progress.value * video.duration) / 100
+}
+
+// Stop video
+function stopVideo() {
+    video.currentTime = 0;
+    video.pause();
+}
